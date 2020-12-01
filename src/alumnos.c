ed3a4657a36293de6f3ec1daae42090e55ecd56e
Cristian <cristian.locatelli@tecme.com.ar>
2020-12-01 12:10:34 -0300

    Se agregan los datos del alumno Locatelli, Cristian

--- src/alumnos.c
+++ src/alumnos.c
@@ -31,8 +31,15 @@ static const struct alumno_s ESTEBAN_VOLENTINI = {
     .documento = "23.517.968",
 };
 
+static const struct alumno_s CRISTIAN_LOCATELLI = {
+    .apellidos = "LOCATELLI",
+    .nombres = "Cristian Angel",
+    .documento = "33.278.404",
+};
+
 const alumno_t ALUMNOS[] = {
     &ESTEBAN_VOLENTINI,
+    &CRISTIAN_LOCATELLI,
 };
 
 const int CANTIDAD_ALUMNOS = (sizeof(ALUMNOS) / sizeof(alumno_t));
